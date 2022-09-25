1e87d46d991a47683a91a135d3365b719127e00c
Stevo <steven.barrios92@gmail.com>
2022-09-24 19:40:16 -0700

    working on notes.js

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,7 @@
 const express = require('express').Router();
 
 const fs = require('fs');
+const router = require('.');
 
 router.get("/", async (req, res) => {
 
@@ -10,6 +11,18 @@ router.get("/", async (req, res) => {
         }else{
             const index = JSON.parse(data)
             res.json(index);
+        }
+    })
+});
+
+router.get("/", async (req, res) => {
+
+    fs.readFile("./db/db.json", "utf-8", (err, data)=>{
+        if(err){
+            res.json(err);
+        }else{
+            const notes = JSON.parse(data)
+            res.json(notes);
         }
     })
 });
