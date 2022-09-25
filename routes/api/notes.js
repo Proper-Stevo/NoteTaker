ede3850672587b9d67157087a91c64fd2507e657
Stevo <steven.barrios92@gmail.com>
2022-09-24 20:50:48 -0700

    more information, delete info

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,28 +1,66 @@
-const express = require('express').Router();
+const router = require('express').Router();
 
 const fs = require('fs');
-const router = require('.');
 
-router.get("/", async (req, res) => {
+router.get("/notes", async (req, res) => {
 
-    fs.readFile("./db/db.json", "utf-8", (err, data)=>{
-        if(err){
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
             res.json(err);
-        }else{
+        } else {
             const index = JSON.parse(data)
             res.json(index);
         }
     })
 });
 
+// Get Notes 
 router.get("/", async (req, res) => {
 
-    fs.readFile("./db/db.json", "utf-8", (err, data)=>{
-        if(err){
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
             res.json(err);
-        }else{
+        } else {
             const notes = JSON.parse(data)
             res.json(notes);
         }
     })
 });
+
+// Display Notes 
+router.post("/", async (req, res) => {
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            res.json(err);
+        } else {
+            const newNote = req.body;
+            const notes = JSON.parse(data);
+            notes.push(newNote);
+            fs.writeFile("./db/db.json", JSON.stringify(notes, null, "\t"), (err) => {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(notes)
+                }
+            })
+
+        }
+    })
+});
+
+router.delete("/:id", async (req, res) => {
+
+    Note.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((deletedNote) => {
+        res.json(deletedNote);
+    })
+    .catch((err) => res.json(err))
+    });
+
+
+module.exports = router;
