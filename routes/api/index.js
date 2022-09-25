768e2754a3b618798981928369687536a9db41bc
Stevo <steven.barrios92@gmail.com>
2022-09-24 22:50:40 -0700

    server stuff

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,8 @@ const routedNotes = require('./notes');
 
 router.use('/notes', routedNotes);
 
+router.get("/notes");
+
+const path = require('path');
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,19 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+const path = require('path');
+
+router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+
 module.exports = router;
